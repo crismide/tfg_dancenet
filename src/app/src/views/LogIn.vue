df7f5795ee04b6751d052eb2acae15ff32c0c563
Cris Milego <crissmilego@gmail.com>
2023-06-26 19:37:31 +0200

    conflictos solucionados

--- src/app/src/views/LogIn.vue
+++ src/app/src/views/LogIn.vue
@@ -9,9 +9,8 @@
                     <input type="password" class="form_login" v-model="user.password" placeholder="Contraseña"/><br><br><br><br>
                     <button type="submit" class="button_entrar">Entrar</button>
                 </form>
-                <p>{{ error }}</p>
             </div>
-            
+            <p>{{ error }}</p>
         </div>
         
     </div>
@@ -30,7 +29,7 @@ export default{
                 email: '',
                 password: ''
             },
-            error:'error'
+            error:''
         }
     },
     methods: {
@@ -50,7 +49,6 @@ export default{
             })
             .catch(error => {
                 console.log(error);
-                this.error = error
             });
         }
     }
@@ -59,50 +57,49 @@ export default{
 
 <style>
 
-.logo{
+.logo {
   background-size: cover;
   background-image: url("../assets/logo_dancenet.png");
   width: 120px;
   height: 120px;
 }
 
-.espaciado_centrado{
-    gap: 76px;
-    align-items: center;
+.espaciado_centrado {
+  gap: 76px;
+  align-items: center;
 }
 
-.form_login{
-    width: 286px;   
-    height: 64px;
-    background: #F4F4F4;
-    border-radius: 5px;
-    color: #7C7C7C;
-    font-size: 16px;
-    
+.form_login {
+  width: 286px;
+  height: 64px;
+  background: #F4F4F4;
+  border-radius: 5px;
+  color: #7C7C7C;
+  font-size: 16px;
 }
 
-.button_entrar{
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    padding: 15px, 91px, 15px, 91px;
-    gap: 10px;
-    border: 0px;
-    width: 286px;
-    height: 58px;
-    
-    border-radius: 25px;
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 20px;
-    line-height: 24px;
-    color: #FFFFFF;
-    background: #C286F1;
+.button_entrar {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 15px 91px 15px 91px;
+  gap: 10px;
+  border: 0px;
+  width: 286px;
+  height: 58px;
+  border-radius: 25px;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 24px;
+  color: #FFFFFF;
+  background: #C286F1;
 }
 
-::placeholder{
-    padding: 19px;
+::placeholder {
+  padding: 19px;
 }
+
 </style>
